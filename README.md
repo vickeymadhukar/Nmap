8ec76fc9bfd6aef62f9b56cf815a6c6427353dad
vickeymadhukar <vickeymadhukar026@gmail.com>
2022-11-12 21:49:38 +0530

    Nmap

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Nmap
 Nmap is network scanning tool which scann your network to find your vulnerability
+vikas
+hellobhai thik he bhai 
---
+++ ascii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int code;
+    char symbol;
+    printf("input the ASCII code (0 to 127): ");
+    scanf("%d",&code);
+    symbol=code;
+    printf("the symbol corresponding to %d is %c\n",code,symbol);
+
+
+
+
+return 0;
+}
